183094a52f9712ef74b4e2c3d570600ffe438ec1
KDY <lemon8818@naver.com>
2024-06-25 21:28:03 +0900

    Design: css 수정 Docs: sql 수정 Feat: jsp 출석부 select 기능, 권한설정

--- src/main/java/com/ddschool/project/classbook/controller/ClassbookSelectListMember.java
+++ src/main/java/com/ddschool/project/classbook/controller/ClassbookSelectListMember.java
@@ -7,17 +7,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet("/classbook/admin")
-public class ClassbookSelectListServlet extends HttpServlet {
+@WebServlet("/classbook/member")
+public class ClassbookSelectListMember extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		String path="/WEB-INF/views/classbook/classbookPage.jsp";
-	
+		
+		
+		String path="/WEB-INF/views/classbook/classbookPageMember.jsp";
+		
 		request.getRequestDispatcher(path).forward(request, response);
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
 
