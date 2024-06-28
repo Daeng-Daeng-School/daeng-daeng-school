63d792a8942bf5aed62ea7192e5b42e0d6d95be8
KDY <lemon8818@naver.com>
2024-06-28 15:02:16 +0900

    Feat:출석부
    출석부 조회 회원 페이지 개발 완료
    
    Resolves: #10, #25

--- src/main/java/com/ddschool/project/member/model/dto/MemberDTO.java
+++ src/main/java/com/ddschool/project/member/model/dto/MemberDTO.java
@@ -6,7 +6,6 @@ public class MemberDTO {
 	
 	private int memberCode;
 	
-<<<<<<< Upstream, based on origin/master
 	private Integer classCode;
 	
 	private int roleCode;
@@ -120,110 +119,6 @@ public class MemberDTO {
 
 	public void setMemberBirth(String memberBirth) {
 		this.memberBirth = memberBirth;
-=======
-	private int classCode;
-	
-	private int roleCode;
-	
-	private String memberId;
-	
-	private String memberPwd;
-	
-	private String memberName;
-	
-	private String phone;
-	
-	private String address;
-	
-	private boolean status;
-	
-	private Date joinDate;
-	
-	private Date withdrawDate;
-	
-	public MemberDTO() {
-		super();
-	}
-
-	public MemberDTO(int memberCode, int classCode, int roleCode, String memberId, String memberPwd, String memberName,
-			String phone, String address, boolean status, Date joinDate, Date withdrawDate) {
-		super();
-		this.memberCode = memberCode;
-		this.classCode = classCode;
-		this.roleCode = roleCode;
-		this.memberId = memberId;
-		this.memberPwd = memberPwd;
-		this.memberName = memberName;
-		this.phone = phone;
-		this.address = address;
-		this.status = status;
-		this.joinDate = joinDate;
-		this.withdrawDate = withdrawDate;
-	}
-
-	public int getMemberCode() {
-		return memberCode;
-	}
-
-	public void setMemberCode(int memberCode) {
-		this.memberCode = memberCode;
-	}
-
-	public int getClassCode() {
-		return classCode;
-	}
-
-	public void setClassCode(int classCode) {
-		this.classCode = classCode;
-	}
-
-	public int getRoleCode() {
-		return roleCode;
-	}
-
-	public void setRoleCode(int roleCode) {
-		this.roleCode = roleCode;
-	}
-
-	public String getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-
-	public String getMemberPwd() {
-		return memberPwd;
-	}
-
-	public void setMemberPwd(String memberPwd) {
-		this.memberPwd = memberPwd;
-	}
-
-	public String getMemberName() {
-		return memberName;
-	}
-
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
->>>>>>> 150794a Design: css 수정 Docs: sql 수정 Feat: jsp 출석부 select 기능, 권한설정
 	}
 
 	public boolean isStatus() {
